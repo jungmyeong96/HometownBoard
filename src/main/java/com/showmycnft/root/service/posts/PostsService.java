56ce90e2dab88eba7ec4e9ee4870ed7c0145ac01
한중명 <gkswndaud@gmail.com>
2023-04-10 21:16:52 +0900

    feat: implements get/put posts API
    POSTS 테이블에 필요한 수정/조회 API 추가 구현
    단위 테스트를 통한 체크 완료
    H2 콘솔을 통한 DB검사 완료

--- src/main/java/com/showmycnft/root/service/posts/PostsService.java
+++ src/main/java/com/showmycnft/root/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.showmycnft.root.service.posts;
 
+import com.showmycnft.root.domain.posts.Posts;
 import com.showmycnft.root.domain.posts.PostsRepository;
+import com.showmycnft.root.web.dto.PostsResponseDto;
 import com.showmycnft.root.web.dto.PostsSaveRequestDto;
+import com.showmycnft.root.web.dto.PostsUpdateRequestDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,4 +19,22 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent()); //영속성 컨텍스트
+
+        return id;
+
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
 }
--- src/main/java/com/showmycnft/root/web/PostsApiController.java
+++ src/main/java/com/showmycnft/root/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.showmycnft.root.web;
 
 import com.showmycnft.root.service.posts.PostsService;
+import com.showmycnft.root.web.dto.PostsResponseDto;
 import com.showmycnft.root.web.dto.PostsSaveRequestDto;
+import com.showmycnft.root.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +17,17 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
     }
 
 }
