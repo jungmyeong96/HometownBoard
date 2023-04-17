307729ce8de570946d42bb1fc0133e3ba61b040d
한중명 <gkswndaud@gmail.com>
2023-04-17 21:22:14 +0900

    feat: implements index page
    머스테치로 기본페이지를 구현하였습니다.

--- src/main/java/com/showmycnft/root/web/IndexController.java
+++ src/main/java/com/showmycnft/root/web/IndexController.java
@@ -1,2 +1,13 @@
-package com.showmycnft.root.web;public class IndexController {
+package com.showmycnft.root.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // mustache 스타터가 앞의 경로와 확장자를 자동으로 지정
+    }
 }
