f039cb16ee4db2e7175d0d8ddb918bc372f39a7c
한중명 <gkswndaud@gmail.com>
2023-04-19 22:18:12 +0900

    feat: implement post act with js
    등록버튼에 대한 동작을 index.js파일에 추가구현하였습니다.

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,30 @@
+var main = { //함수 선언으로 index.js 만의 유효범위 scope 만들기
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+   save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
