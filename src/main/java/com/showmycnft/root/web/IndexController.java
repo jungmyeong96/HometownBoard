fde4bd3a629272d954774e9d6f8028a0f9a58b82
한중명 <gkswndaud@gmail.com>
2023-04-19 22:01:54 +0900

    feat: implement basic input template
    부트스트랩과 제이쿼리를 이용하기 기본적인 입력 템플릿 생성

--- src/main/java/com/showmycnft/root/web/IndexController.java
+++ src/main/java/com/showmycnft/root/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index"; // mustache 스타터가 앞의 경로와 확장자를 자동으로 지정
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save"; //posts-save.mustache 호출
+    }
 }
