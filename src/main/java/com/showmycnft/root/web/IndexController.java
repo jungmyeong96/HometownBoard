cd8425ceda187a89209fb0941ddb76a234ba8ff2
한중명 <gkswndaud@gmail.com>
2023-04-20 22:10:34 +0900

    feat: implements all select table
    - transactional readonly를 통한 성능개선
    - Model을 사용하여 서버 템플릿 엔진에 객체 저장
    - / controller 수정

--- src/main/java/com/showmycnft/root/web/IndexController.java
+++ src/main/java/com/showmycnft/root/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.showmycnft.root.web;
 
+import com.showmycnft.root.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다!
+        model.addAttribute("posts", postsService.findAllDesc()); //데이터를 posts에 담아 저장
         return "index"; // mustache 스타터가 앞의 경로와 확장자를 자동으로 지정
     }
 
