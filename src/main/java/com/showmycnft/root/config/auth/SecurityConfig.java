1d97de2ccb53469b316bce2f930b5978dcaa5320
한중명 <gkswndaud@gmail.com>
2023-05-11 22:41:20 +0900

    feat: implements Security config for SSO
    - SSO 설정을 위해 Security config설정을 구성하였습니다.
    - Session User를 통해 직렬화된 데이터를 다룹니다.
    - 로그인 성공할시 후속저치를 위한 CustomOAuth2UserService 클래스 생성

--- src/main/java/com/showmycnft/root/config/auth/SecurityConfig.java
+++ src/main/java/com/showmycnft/root/config/auth/SecurityConfig.java
@@ -2,24 +2,29 @@ package com.showmycnft.root.config.auth;
 
 import com.showmycnft.root.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity //Spring Security 설정을 활성화하는 어노테이션
+public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .headers().frameOptions().disable()
-                .and().authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**")
-                .permitAll().antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
-                .and().logout().logoutSuccessUrl("/")
-                .and().oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
-        }
 
-        }
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable() //h2 console 화면을 사용하기 위해 해당 옵션들을 사용하지 않을 것 입니다.
+                .and().authorizeRequests() //URL별 권한 관리를 설정하는 옵션, antMatchers옵션 사용하기 위함
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() //권한 관리 대상을 지정
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //RBAC
+                .anyRequest().authenticated() //나머지 처리 authenticated()를 추가해서 나머지는 모두 인증된 사용자들에게만 허용하게함
+                .and().logout().logoutSuccessUrl("/") //로그아웃 기능에 대한 여러 설정의 진입점이고 로그아웃 시 /로 이동
+                .and().oauth2Login() //
+                .userInfoEndpoint() //OAuth 2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
+                .userService(customOAuth2UserService); // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
+        return http.build();
+    }
+}
---
+++ src/main/java/com/showmycnft/root/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,53 @@
+package com.showmycnft.root.config.auth.dto;
+
+import com.showmycnft.root.domain.user.Role;
+import com.showmycnft.root.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes,
+                           String nameAttributeKey, String name,
+                           String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes); //Map 형태 하나하나 반환
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName,
+                                            Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() { //user 엔티티 생성 후, GUEST처리
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
